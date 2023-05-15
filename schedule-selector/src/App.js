8a6dbe742bfd4f81666a5337b53cedb6a1f263b4
Anokhi <ashah13@cs.washington.edu>
2023-05-15 13:53:35 -0700

    added schedule-selector code to react app

--- schedule-selector/src/App.js
+++ schedule-selector/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import ScheduleSelector from 'react-schedule-selector'
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { schedule : [] }
+
+  handleChange = newSchedule => {
+    console.log(newSchedule)
+    this.setState({ schedule: newSchedule })
+  }
+
+
+
+  render() {
+    return (
+      <ScheduleSelector
+        selection={this.state.schedule}
+        numDays={14}
+        minTime={8}
+        maxTime={22}
+        hourlyChunks={2}
+        onChange={this.handleChange}
+      />
+    )
+  }
 }
 
 export default App;

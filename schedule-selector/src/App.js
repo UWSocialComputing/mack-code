92285caecb4d029f9d9674c36f94c979509c38d7
kristinalansang <42708544+kristinalansang@users.noreply.github.com>
2023-05-16 14:37:04 -0700

    added buttons

--- schedule-selector/src/App.js
+++ schedule-selector/src/App.js
@@ -5,6 +5,53 @@ import axios from 'axios'
 
 const url='http://127.0.0.1:5001/friendstomeet-155ac/us-central1/getPlans'
 
+function SaveButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Save
+    </button>
+  );
+}
+
+function PhoneButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Save
+    </button>
+  );
+}
+
+function NumNotifButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Save
+    </button>
+  );
+}
+
+function DaysNotifButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Save
+    </button>
+  );
+}
 
 class App extends React.Component {
   state = { schedule : [] }
@@ -18,9 +65,10 @@ class App extends React.Component {
   }
 
 
-
   render() {
     return (
+      <div>
+      <h> Friends2Meet</h>
       <ScheduleSelector
         selection={this.state.schedule}
         numDays={14}
@@ -29,6 +77,32 @@ class App extends React.Component {
         hourlyChunks={2}
         onChange={this.handleChange}
       />
+      <SaveButton />
+      <body> Enter your phone number: </body>
+      <input
+            id = "phone"
+            label = "phone"
+            type="text"
+            value={this.state.value}
+         />
+      <PhoneButton />
+      <body> Enter number of times you would like to be notified of a hangout: </body>
+      <input
+            id = "numnotif"
+            label = "numnotif"
+            type="text"
+            value={this.state.value}
+         />
+      <NumNotifButton />
+      <body> Enter number of days in advance you would like to be notified: </body>
+      <input
+            id = "daysnotif"
+            label = "daysnotif"
+            type="text"
+            value={this.state.value}
+         />
+      <DaysNotifButton />
+      </div>
     )
   }
 }
